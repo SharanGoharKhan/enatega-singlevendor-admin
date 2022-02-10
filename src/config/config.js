abbc4bf0581aaeea564c8274c4881570a4e8d337
Sharan Khan <sharan.gohar@gmail.com>
2022-02-10 18:32:16 +0500

    added files

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+/* eslint-disable camelcase */
+export const server_url = 'http://localhost:8000/'
+export const ws_server_url = 'ws://localhost:8000/'
+export const cloudinary_upload_url =
+  'https://api.cloudinary.com/v1_1/dimjm4ald/image/upload'
+export const cloudinary_category = 'hmtkg7s5'
+export const cloudinary_food = 'wdgvyas8'
